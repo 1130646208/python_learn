96e288f57ffb971d760c019947a12280521ba359
wangshuangxing <1130646208@qq.com>
2019-10-09 23:57:17 +0800

    网络编程练习

--- zhengze_1.py
+++ zhengze_1.py
@@ -1,13 +1,22 @@
 import re
- 
+
 line = "Cats are smarter than dogs"
 # .* 表示任意匹配除换行符（\n、\r）之外的任何单个或多个字符
-matchObj = re.match( r'(.*) are (.*?) .*', line, re.M|re.I)
- 
+matchObj = re.match(r'(.*) are (.*?) .*', line, re.M | re.I)
+
 if matchObj:
-   print ("matchObj.group() : ", matchObj.group())
-   print ("matchObj.group(1) : ", matchObj.group(1))
-   print ("matchObj.group(2) : ", matchObj.group(2))
-   print ("matchObj.groups() : ", matchObj.groups())
+    print("matchObj.group() : ", matchObj.group())
+    print("matchObj.group(1) : ", matchObj.group(1))
+    print("matchObj.group(2) : ", matchObj.group(2))
+    print("matchObj.groups() : ", matchObj.groups())
+
 else:
-   print ("No match!!")
+    print("No match!!")
+
+pattern = re.compile(r'\d+')  # 查找数字
+result0 = re.findall(r'\d+', 'runoob 123 google 456')
+result1 = pattern.findall('runoob 123 google 456')
+result2 = pattern.findall('run88oob123google456', 0, 10)
+print(result0)
+print(result1)
+print(result2)
